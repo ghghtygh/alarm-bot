2c2356341e3f033dada4482cfda98353129c0386
jupo13 <ghghtygh@naver.com>
2023-05-22 23:42:31 +0900

    share project

--- core/src/main/java/org/gpg/alarm/core/domain/telegram/repository/BotRepository.java
+++
@@ -1,7 +0,0 @@
-package org.gpg.alarm.core.domain.telegram.repository;
-
-import org.gpg.alarm.core.domain.telegram.entity.Bot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BotRepository extends JpaRepository<Bot, Long> {
-}
